31c2e0ebb7501ce3bb4cadcd5c98e0054a63a64c
alok328 <40838760+alok328@users.noreply.github.com>
2020-06-02 23:19:29 +0530

    added server code

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.listen(PORT,()=>{
+    console.log('started!');
+})
